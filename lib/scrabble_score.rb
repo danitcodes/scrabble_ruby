9c77fe8d3cd135c068e1180fca38ede64e7b2a01
Danielle Thompson <danithompson74@gmail.com>
2021-06-24 15:10:13 -0700

    Sorting out wk 1 lsn 5 Epicodus notes

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,5 +1,60 @@
 require('pry')
 
-def scrabble_score(string)
-  #code here
+class Word
+  # initialize method runs any code as soon as an object is created
+  def initialize(word)
+    @word = word
+  end
+
+  # a reader method
+  # @TODO - should be tested, too!
+  #needed to *save* value of user input that went first through `initialize`
+  def word
+    @word
+  end
+
+  # def letter_score_assignments
+  #   letter_scores = {
+  #     "A" => 1,
+  #     "E" => 1,
+  #     "I" => 1,
+  #     "O" => 1,
+  #     "U" => 1,
+  #     "L" => 1,
+  #     "N" => 1,
+  #     "R" => 1,
+  #     "S" => 1,
+  #     "T" => 1,
+  #     "D" => 2,
+  #     "G" => 2,
+  #     "B" => 3,
+  #     "C" => 3,
+  #     "M" => 3,
+  #     "P" => 3,
+  #     "F" => 4,
+  #     "H" => 4,
+  #     "V" => 4,
+  #     "W" => 4,
+  #     "Y" => 4,
+  #     "K" => 5,
+  #     "J" => 8,
+  #     "X" => 8,
+  #     "Q" => 10,
+  #     "Z" => 10
+  #   }
+  # end
+
+  # def scrabble_score(string)
+  #   string_array = str.split("")
+  #   score_array = []
+  #   string_array.each do |letter|
+  #     if #the character is an alphabet letter
+  #       score_array.push(#the letter's value)
+  #     else
+  #       #do nothing & move to next character
+  #     end
+  #   end
+  #   word_score = string_array.sum()
+  #   return word_score
+  # end
 end
